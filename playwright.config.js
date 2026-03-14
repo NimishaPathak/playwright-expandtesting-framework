207f1b582d4cf68b1d351f30b882e69da4aae799
nimisha <ndesai2mail@gmail.com>
2026-03-14 16:13:52 +0530

    Added playwright configuration with parallel execution

---
+++ playwright.config.js
@@ -0,0 +1,21 @@
+import { defineConfig } from '@playwright/test';
+import { getEnv } from './configs/envConfig.js';
+
+const env = getEnv();
+
+export default defineConfig({
+    testDir: './tests',
+    timeout: 30 * 1000,
+    retries: 1,
+    use: {
+        baseURL: env.baseURL,
+        headless: false,
+        screenshot: 'only-on-failure',
+        video: 'retain-on-failure'
+    },
+    reporter: [
+        ['list'],
+        ['html']
+    ],
+    fullyParallel: true
+})
