e95b3937cf2e5678d953e375e33a19ee95d20618
nimisha <ndesai2mail@gmail.com>
2026-03-15 22:44:44 +0530

    Add LoginPage POM implementation

--- pages/LoginPage.js
+++ pages/LoginPage.js
@@ -7,7 +7,9 @@ export default class LoginPage extends BasePage {
         this.usernameInput = page.locator('#username');
         this.passwordInput = page.locator('#password');
         this.loginBtn = page.locator('button[type="submit"]');
+        this.successMsg = page.locator('.alert-success');
     }
+
     async login(username, password) {
         await this.fill(this.usernameInput, username);
         await this.fill(this.passwordInput, password);
