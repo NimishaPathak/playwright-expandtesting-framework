0323705346ea41a02d8d885efd84a555dbe204af
nimisha <ndesai2mail@gmail.com>
2026-03-15 15:17:48 +0530

    Added environment configuration handling

---
+++ configs/envConfig.js
@@ -0,0 +1,23 @@
+import dotenv from 'dotenv';
+import path from 'path';
+import fs from 'fs';
+import { error } from 'console';
+
+export function getEnv() {
+    const envName = process.env.TEST_ENV || 'dev';
+    const envPath = path.resolve(`configs/${envName}.env`);
+
+    if (!fs.existsSync(envPath)) {
+        throw new Error(`Environment file not found: ${envPath}.env`);
+
+    }
+
+    dotenv.config({
+        path: envPath
+    })
+    return {
+        baseURL: process.env.BASE_URL,
+        username: process.env.USERNAME,
+        password: process.env.PASSWORD
+    }
+}
